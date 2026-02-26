f35af13bdd5d007c459ba584357d419633107385
abhay <shekhawaabhay80@gmail.com>
2026-02-27 00:48:08 +0530

    add frontend domain to the cors

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,8 @@ app.use(cors({
   origin: [
     'http://localhost:3000',   
     'http://localhost:5173',     
-    'http://127.0.0.1:3000',  
+    'http://127.0.0.1:3000',
+    "https://learnify.netlify.app"
   ],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'], 
   allowedHeaders: ['Content-Type', 'Authorization'], 
